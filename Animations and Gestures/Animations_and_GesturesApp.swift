8980f622a497e74169ddb52c8d7e5706736c0137
kylekaufman <ktk33@case.edu>
2025-03-21 10:28:32 -0400

    Initial Commit

---
+++ Animations and Gestures/Animations_and_GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_and_GesturesApp.swift
+//  Animations and Gestures
+//
+//  Created by Kyle Kaufman on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Animations_and_GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
